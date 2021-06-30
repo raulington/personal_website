bea42eab9464c92cd1c3be1f22907547df7fa685
Raul Higareda <rauld@Rauls-MacBook-Pro.local>
2021-06-30 15:03:09 -0500

    Added animations

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
+
+const faders = document.querySelectorAll(".fade-in")
+const sliders = document.querySelectorAll(".slide-in")
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -150px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries, 
+    appearOnScroll
+) {
+    entries.forEach(entry =>{
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
+
